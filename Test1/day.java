b5620d94b9c3cbb9d90021e819f28dd23cf2b22d
Yeseo0502 <5252yeseo@gmail.com>
2025-07-22 06:23:46 +0000

    시작 날짜와 끝 날짜의 사이 날짜구하기

---
+++ Test1/day.java
@@ -0,0 +1,27 @@
+class day {
+	public int func_a(int month, int day){
+        int monthList[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int total = 0;
+        for(int i = 0; i< month-1; i++)
+            total += monthList[i];
+        total +=day;
+        return total - 1;
+    }
+	public int solution(int startMonth, int startDay, int endMonth, int endDay) {
+        int startTotal = func_a(startMonth, startDay);
+        int endTotal = func_a(endMonth, endDay);
+        return endTotal - startTotal;
+    }
+
+    // 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다.
+    public static void main(String[] args) {
+    	day sol = new day();
+        int startMonth = 1;
+        int startDay = 2;
+        int endMonth = 2;
+        int endDay = 2;
+        int ret = sol.solution(startMonth, startDay, endMonth, endDay);
+        
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+    }
+}
