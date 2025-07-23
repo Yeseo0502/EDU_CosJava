7bdf239e3d4efb546b9572b229b5cfc8996dd830
2216_정예서 <s2450@e-mirim.hs.kr>
2025-07-23 02:15:37 +0000

    팰린드롬 판단하기

---
+++ Test2/Main8.java
@@ -0,0 +1,21 @@
+public class Main8 {
+	public int solution(int number) {
+        int count = 0;
+//        while(number >= 0){
+        while(number > 0) {
+            int n = number % 10;
+            if (n == 2 || n == 3 || n == 5 || n == 7)
+                count += 1;
+            number /= 10;
+        }
+        return count;
+    }
+	public static void main(String[] args) {
+		Main8 sol = new Main8();
+        int number = 29022531;
+        int ret = sol.solution(number);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+    }
+}
