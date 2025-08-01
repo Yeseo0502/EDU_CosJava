05ccb37807f99ace6e5071ec58fc9079e2bea507
2216_정예서 <s2450@e-mirim.hs.kr>
2025-08-01 14:26:11 +0000

    저는 따뜻한 날이 좋아요

---
+++ Test6/Main6_01.java
@@ -0,0 +1,21 @@
+class Main6_01{
+    public int solution(int[] temperature, int A, int B) {
+        
+		int answer = 0;
+		for(int i = A; i < B; i++) {
+			if(temperature[i] > temperature[A] && temperature[i] > temperature[B])
+				answer += 1;
+		}
+		return answer;
+	}
+    public static void main(String[] args) {
+		Main6_01 sol = new Main6_01();
+		int[] temperature = {3, 2, 1, 5, 4, 3, 3, 6};
+		int A = 1;
+		int B = 6;
+		int ret = sol.solution(temperature, A, B);
+
+		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+		System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+	}
+}
